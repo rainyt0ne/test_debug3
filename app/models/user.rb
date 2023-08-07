60f43e1119b487e52732fc2d62ee5197bec410d2
rainyt0ne <138280990+rainyt0ne@users.noreply.github.com>
2023-08-07 01:16:46 +0000

    commit2

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,9 @@ class User < ApplicationRecord
   has_one_attached :profile_image
 
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-    has_many :followings, through: active_relationships, source: :followed
+    has_many :followings, through: :active_relationships, source: :followed
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-    has_many :followers, through: passive_relationships, source: :follower
+    has_many :followers, through: :passive_relationships, source: :follower
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
